ef2644c94c5a73d0884259a55f1aa4b6e0b3054e
nsf <no.smile.face@gmail.com>
2010-12-16 07:58:20 +0500

    Update Go documentation. Release 2010-12-15.

---
+++ go/gortfm-runtime_cgo-data.js
@@ -0,0 +1,1 @@
+var gortfmData = {index:"index.html",html:"<p>\nPackage cgo contains runtime support for code generated\nby the cgo tool.  See the documentation for the cgo command\nfor details on using cgo.\n</p>\n",name:"cgo",types:[],funcs:[],consts:[],vars:[]}
